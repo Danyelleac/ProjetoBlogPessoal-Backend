a806294ca92d676d147004ff602687cdb8aa533a
DanyelleAC <danyelleacandido@gmail.com>
2022-04-11 12:26:03 -0300

    deploy do Projeto

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
@@ -27,8 +29,9 @@ public class Usuario {
 	@NotNull(message = "Olá, inserir o nome é obrigatório!")
 	private String nome;
 
-	@NotNull(message = "Olá, inserir o usuário é obrigatório!")
-	@Email(message = "E-mail invalido, insira um email correto!")
+	@Schema(example = "email@email.com.br")
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
 
 	@NotBlank(message = "Olá, inserir a senha é obrigatório!")
